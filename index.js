543f30c95c5b7654fd9c5621602fb7f276e084e1
Sopl'Wang <sopl.wang@gmail.com>
2013-02-12 16:45:56 +0800

    Add TODO task...

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Flow control
 // TODO: Feedback services
 // TODO: APNS invalid token need add to feedbacks
+// TODO: use node-restify
 
 exports.mqtt = require('./lib/mqtt')
 exports.apns = require('./lib/apns');
