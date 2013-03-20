c641236f89ea4fa9deab301111908d2f5d0df5a6
Sopl'Wang <sopl.wang@gmail.com>
2013-03-20 18:56:21 +0800

    APNS Workaround of Node's pipe bug - 2

--- examples/notific.js
+++ examples/notific.js
@@ -18,14 +18,14 @@ var data = {
   },
 };
 
+for (var i = 0; i < 1000; i++) {
+  data.tokens.push(token);
+}
+
 var opts = {
   path: '/ios/notific/com.hupu.GameMate',
   headers: {'Connection': 'close'},
 };
-
-for (var i = 0; i < 999; i++) {
-  data.tokens.push(token);
-}
 
 client.post(opts, data, function (err, req, res, obj) {
   assert.ifError(err);
